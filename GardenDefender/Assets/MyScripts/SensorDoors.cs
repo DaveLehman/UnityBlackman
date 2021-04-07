fd9c4b0eed6aa96d136ce40e2bef2f505e61553b
DaveLehman <lemo@provide.net>
2021-04-07 13:46:17 -0400

    Doors and ZombieBunny

---
+++ GardenDefender/Assets/MyScripts/SensorDoors.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SensorDoors : MonoBehaviour {
+    public AnimationClip clipOpen;
+    public AnimationClip clipClose;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter (Collider defender)
+    {
+        if (defender.gameObject.tag == "Player")
+        {
+            animation.Play(clipOpen.name);
+            audio.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider defender)
+    {
+        if (defender.gameObject.tag == "Player")
+        {
+            animation.Play(clipClose.name);
+            audio.Play();
+        }
+    }
+
+}
