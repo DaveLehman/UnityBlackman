b7f04d2ee97ebb18ea59516c50216b8166d16854
DaveLehman <lemo@provide.net>
2021-04-12 06:26:37 -0400

    Battery implemented, game over started

---
+++ Ch09/Assets/MyScripts/BatteryHealth.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BatteryHealth : MonoBehaviour {
+    public float batteryFull = 70.0f;   // number of seconds left in battery
+    float batteryRemaining;              // remaining battery life in seconds
+    int percentRemaining;
+    bool trackingBattery = true;               // timer started/not started
+    public Text guiText;
+
+	// Use this for initialization
+	void Start () {
+	    // find the HUD and set the starting batter values
+        batteryRemaining = batteryFull;
+        guiText.color = Color.green;
+        guiText.text = "Battery: " + percentRemaining.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (trackingBattery)
+        {
+            if (batteryRemaining > 0)
+            {
+                batteryRemaining -= Time.deltaTime; // second countdown
+                percentRemaining = (int)((batteryRemaining / batteryFull) * 100);
+                if(percentRemaining <= 10)
+                {
+                    guiText.color = Color.red;
+                }
+                else
+                {
+                    guiText.color = Color.green;
+                }
+                guiText.text = "Battery: " + percentRemaining.ToString();
+            }
+            else
+            {
+                GameOver();
+                trackingBattery = false;
+            }
+        }
+	}
+
+    private void GameOver()
+    {
+        print("Game Over");
+        // deactivate the potato gun
+        GameObject.Find("Fire Point").SetActive(false);
+        // disable navigation
+        GameObject.Find("Gnomatic Garden Defender").GetComponent<CharacterMotor>().enabled = false;
+        // disable turning
+        GameObject.Find("Gnomatic Garden Defender").GetComponent<MouseLook>().enabled = false;
+    }
+}
